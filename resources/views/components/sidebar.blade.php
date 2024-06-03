a04ed9bab73bd853e154b640f267ea742adb5881
Dimas Pratikto <dimaspratikto29@gmail.com>
2024-06-03 22:14:47 +0700

    permissions & note

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -23,5 +23,14 @@
                     <span>Company</span></a>
             </li>
 
+            <li class="nav-item ">
+                <a href="{{ route('attendances.index') }}" class="nav-link "><i class="fas fa-columns"></i>
+                    <span>Absensi</span></a>
+            </li>
+
+            <li class="nav-item ">
+                <a href="{{ route('permissions.index') }}" class="nav-link "><i class="fas fa-columns"></i>
+                    <span>Permission</span></a>
+            </li>
     </aside>
 </div>
